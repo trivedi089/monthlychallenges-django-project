45872b817d6fdebf760f618a6de15734e8fe698b
trivedi089 <satyachbs007@gmail.com>
2025-12-21 18:54:32 +0530

    dynamic-url

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from challenges import views
 
 urlpatterns = [
-    path('january/',views.index,name='index'),
+    #path('january/',views.index,name='index'),
+    #path('feburuary/',views.feb_challenge,name='index'),
+
+    path("<str:month>/", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -2,5 +2,19 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Exercise daily forn 20 minutes")
+    return HttpResponse("Exercise daily for 20 minutes")
 
+def feb_challenge(request):
+    return HttpResponse("No fast food for entire month")
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text =  "Exercise daily for 20 minutes"
+    elif month == "feburaury":
+        challenge_text = "No fast food for entire month"
+    elif month == "march":
+        challenge_text = "Wake up early and do meditation"
+    else :
+        challenge_text = "This month was not supported!"
+    return HttpResponse(challenge_text)
